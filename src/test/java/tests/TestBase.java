e3b795e8a4a7a50242db0b66f8ed11d5e51ad84b
Nikita Ryazanov <riazanovnikita@gmail.com>
2025-10-19 17:13:04 +0300

    added demoqa class

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,19 +1,23 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
 
     @BeforeAll
     static void setAll(){
+        Configuration.baseUrl = "https://demoqa.com";
         RestAssured.baseURI = "https://demoqa.com/";
     }
 
-   @BeforeEach
+   @AfterEach
     void setUp(){
-
+        closeWebDriver();
    }
 
 }
